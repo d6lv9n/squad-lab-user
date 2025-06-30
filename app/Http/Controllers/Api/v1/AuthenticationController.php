1467263e7ab6c8931198742fdb6ef7d555488a15
Delvin <work.d6lv9n.github@gmail.com>
2025-07-01 00:10:34 +0700

    Create Login and Signup request and implemented on AuthenticationController

--- app/Http/Controllers/Api/v1/AuthenticationController.php
+++ app/Http/Controllers/Api/v1/AuthenticationController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\v1\LoginRequest;
+use App\Http\Requests\Api\v1\SignupRequest;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
-    //
+    public function login(LoginRequest $request)
+    {
+
+    }
+
+    public function profile(Request $request)
+    {
+
+    }
+
+    public function signup(SignupRequest $request)
+    {
+
+    }
 }
