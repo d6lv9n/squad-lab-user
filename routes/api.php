8ded5a1c023e89dbd2c5a18d7032259789047b74
Delvin <work.d6lv9n.github@gmail.com>
2025-07-12 01:19:13 +0700

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthenticationController;
+use App\Http\Controllers\Api\v1\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->middleware(['throttle:100,1'])->group(function () {
+    Route::prefix('account')->group(function () {
+        Route::get('profile', [AuthenticationController::class, 'profile']);
+        Route::post('login', [AuthenticationController::class, 'login']);
+        Route::post('signup', [AuthenticationController::class, 'signup']);
+        // Route::post('refresh-token', [AuthenticationController::class, 'refreshToken']);
+    });
+
+    Route::prefix('users')->group(function () {
+        Route::get('get-by-ids', [UsersController::class, 'getByIds']);
+        Route::get('search/{query}', [UsersController::class, 'search']);
+    });
+});
