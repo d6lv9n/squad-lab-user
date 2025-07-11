4d849f335ce167f06eff580d45c64a4de9065e04
Delvin <work.d6lv9n.github@gmail.com>
2025-07-12 00:53:04 +0700

    First commit

---
+++ app/Http/Controllers/Api/v1/UsersController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\v1\GetByIdsRequest;
+use App\Http\Requests\Api\v1\ValidateUserIdsRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function getByIds(GetByIdsRequest $request): JsonResponse
+    {
+        $users = User::query()
+                ->select('id', 'name')
+                ->whereNull('deleted_at')
+                ->whereIn('id', $request->ids)
+                ->get();
+
+        return response()->json([
+            'users' => $users
+        ], 200);
+    }
+
+    public function search(Request $request): JsonResponse
+    {
+        // 
+    }
+}
