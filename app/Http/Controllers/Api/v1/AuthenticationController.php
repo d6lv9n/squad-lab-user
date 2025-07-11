9416695ac7b6cf0ef14b163cde8a2b74d0335051
Delvin <work.d6lv9n.github@gmail.com>
2025-07-12 00:44:13 +0700

    Add controller's middleware and some methods

--- app/Http/Controllers/Api/v1/AuthenticationController.php
+++ app/Http/Controllers/Api/v1/AuthenticationController.php
@@ -5,22 +5,74 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\v1\LoginRequest;
 use App\Http\Requests\Api\v1\SignupRequest;
+use App\Repositories\AuthenticationRepositoryInterface;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class AuthenticationController extends Controller
+class AuthenticationController extends Controller implements HasMiddleware
 {
-    public function login(LoginRequest $request)
-    {
+    private $authenticationRepositoryInterface;
 
+    public function __construct(
+        AuthenticationRepositoryInterface $authenticationRepositoryInterface,
+    ) {
+        $this->authenticationRepositoryInterface = $authenticationRepositoryInterface;
     }
 
-    public function profile(Request $request)
+    /**
+     * Get the middleware that should be assigned to the controller.
+     */
+    public static function middleware(): array
     {
-
+        return [
+            new Middleware('auth:api', only: ['logout', 'profile']),
+            new Middleware('guest:api', only: ['login', 'signup']),
+        ];
     }
 
-    public function signup(SignupRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
+        $cacheKey = 'login:' . sha1($request->ip() . '|' . $request->email);
 
+        $attempts = $this->authenticationRepositoryInterface->checkAttempts($cacheKey);
+
+        $user = $this->authenticationRepositoryInterface->findUserAndVerifyPassword($request->only(['email', 'password']), $cacheKey, $attempts);
+
+        $token = $this->authenticationRepositoryInterface->createToken($user);
+
+        $this->authenticationRepositoryInterface->clearAttempts($cacheKey);
+
+        return response()->json([
+            'profile' => $user,
+            'token' => $token
+        ], 200);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $this->authenticationRepositoryInterface->logoutUser();
+
+        return response()->json(null, 204);
+    }
+
+    public function profile(Request $request): JsonResponse
+    {
+        return response()->json([
+            'profile' => auth()->user()
+        ], 200);
+    }
+
+    public function refreshToken(Request $request): JsonResponse
+    {
+        return response()->json([
+            'token' => auth()->refresh(),
+        ]);
+    }
+
+    public function signup(SignupRequest $request): JsonResponse
+    {
+        
     }
 }
