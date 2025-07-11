7f639ffc6c3133383aa544a85b28a75e5b6fd17f
Delvin <work.d6lv9n.github@gmail.com>
2025-07-12 01:17:13 +0700

    Remove unused request from controller

--- app/Http/Controllers/Api/v1/UsersController.php
+++ app/Http/Controllers/Api/v1/UsersController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\v1\GetByIdsRequest;
-use App\Http\Requests\Api\v1\ValidateUserIdsRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
